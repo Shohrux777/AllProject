153f3e7b5e23ee8f2ab4e6d9d74f715540d842b8
Shohrux777 <76987941+Shohrux777@users.noreply.github.com>
2025-10-13 17:52:22 +0500

    dsdasda

--- Tegirmons/vue.config.js
+++ Tegirmons/vue.config.js
@@ -1,7 +1,27 @@
+const fs = require('fs')          // 👈 bu qatorni unutma
+const path = require('path')
 module.exports = {
   devServer: {  
-    https: true,
+    https: {
+      key: fs.readFileSync(path.join(__dirname, 'tegirmon-key.pem')),
+      cert: fs.readFileSync(path.join(__dirname, 'tegirmon.pem')),
+    },
+    historyApiFallback: {
+      disableDotRule: true,
+      rewrites: [
+        { from: /^\/manifest\.json$/, to: '/manifest.json' }
+      ]
+    },
     disableHostCheck: true
+  },
+  pwa: {
+    name: 'Tegirmon CRM',
+    themeColor: '#171717',
+    msTileColor: '#171717',
+    manifestOptions: {
+      start_url: '/',
+      display: 'standalone',
+    },
   },
   pluginOptions: {
     i18n: {
